f12c8a39ac555ff5674059a3a03d38f1b3424fd4
Dilay CANITEZ <dilay.canitez@one37id.com>
2022-08-31 10:59:39 +0300

    groupByCustom fnc added

--- odev2.js
+++ odev2.js
@@ -6,254 +6,271 @@
  * return ettiği değere göre gruplanacak
  * (örneğin :people.groupByCustom( (item)=>item.name[0] ) dediğimde name fieldinin baş harfine göre gruplama yapacak)
  * (örneğin :people.groupByCustom( (item)=>item.age ) dediğimde age fielde göre gruplama yapacak)
- * 
+ *
  */
 
 //İpucu 1: Array.prototype.groupByCustom =  dedikten sonra metodunuzu yazabilirsiniz
-
-//örnek array
-
-const array = [
+Array.prototype.groupByCustom = function (callback) {
+    //use the callback to get the keys of the object
+    const groups = this.map(callback);
+    //use reduce fnc
+    console.log(
+      this.reduce((prev, curr, index) => {
+        let key = groups[index];
+        if (!prev[key]) {
+          prev[key] = [];
+        }
+        prev[key].push(curr);
+        return prev;
+      }, {})
+    );
+  };
+  //örnek array
+  
+  const array = [
     {
-        "name": "Marge Simpson",
-        "gender": "f"
+      name: "Marge Simpson",
+      gender: "f",
     },
     {
-        "name": "Bart Simpson",
-        "gender": "m"
+      name: "Bart Simpson",
+      gender: "m",
     },
     {
-        "name": "Lisa Simpson",
-        "gender": "f"
+      name: "Lisa Simpson",
+      gender: "f",
     },
     {
-        "name": "Moe Szyslak",
-        "gender": "m"
+      name: "Moe Szyslak",
+      gender: "m",
     },
     {
-        "name": "Seymour Skinner",
-        "gender": "m"
+      name: "Seymour Skinner",
+      gender: "m",
     },
     {
-        "name": "Ned Flanders",
-        "gender": "m"
+      name: "Ned Flanders",
+      gender: "m",
     },
     {
-        "name": "Grampa Simpson",
-        "gender": "m"
+      name: "Grampa Simpson",
+      gender: "m",
     },
     {
-        "name": "Chief Wiggum",
-        "gender": "m"
+      name: "Chief Wiggum",
+      gender: "m",
     },
     {
-        "name": "Milhouse Van Houten",
-        "gender": "m"
+      name: "Milhouse Van Houten",
+      gender: "m",
     },
     {
-        "name": "Waylon Smithers",
-        "gender": "m"
+      name: "Waylon Smithers",
+      gender: "m",
     },
     {
-        "name": "Nelson Muntz",
-        "gender": "m"
+      name: "Nelson Muntz",
+      gender: "m",
     },
     {
-        "name": "Edna Krabappel-Flanders",
-        "gender": "f"
+      name: "Edna Krabappel-Flanders",
+      gender: "f",
     },
     {
-        "name": "Announcer",
-        "gender": "m"
+      name: "Announcer",
+      gender: "m",
     },
     {
-        "name": "Selma Bouvier",
-        "gender": "f"
+      name: "Selma Bouvier",
+      gender: "f",
     },
     {
-        "name": "Barney Gumble",
-        "gender": "m"
+      name: "Barney Gumble",
+      gender: "m",
     },
     {
-        "name": "Patty Bouvier",
-        "gender": "f"
+      name: "Patty Bouvier",
+      gender: "f",
     },
     {
-        "name": "Martin Prince",
-        "gender": "m"
+      name: "Martin Prince",
+      gender: "m",
     },
     {
-        "name": "Otto Mann",
-        "gender": "m"
+      name: "Otto Mann",
+      gender: "m",
     },
     {
-        "name": "Lou",
-        "gender": "m"
+      name: "Lou",
+      gender: "m",
     },
     {
-        "name": "Todd Flanders",
-        "gender": "m"
+      name: "Todd Flanders",
+      gender: "m",
     },
     {
-        "name": "Dr. Marvin Monroe",
-        "gender": "m"
+      name: "Dr. Marvin Monroe",
+      gender: "m",
     },
     {
-        "name": "Dr. J. Loren Pyror",
-        "gender": "m"
+      name: "Dr. J. Loren Pyror",
+      gender: "m",
     },
     {
-        "name": "Dewey Largo",
-        "gender": "m"
+      name: "Dewey Largo",
+      gender: "m",
     },
     {
-        "name": "Eddie",
-        "gender": "m"
+      name: "Eddie",
+      gender: "m",
     },
     {
-        "name": "Teacher",
-        "gender": "m"
+      name: "Teacher",
+      gender: "m",
     },
     {
-        "name": "Clerk",
-        "gender": "m"
+      name: "Clerk",
+      gender: "m",
     },
     {
-        "name": "Father",
-        "gender": "m"
+      name: "Father",
+      gender: "m",
     },
     {
-        "name": "Sherri Mackleberry",
-        "gender": "f"
+      name: "Sherri Mackleberry",
+      gender: "f",
     },
     {
-        "name": "JANEY",
-        "gender": "f"
+      name: "JANEY",
+      gender: "f",
     },
     {
-        "name": "Ms. Melon",
-        "gender": "f"
+      name: "Ms. Melon",
+      gender: "f",
     },
     {
-        "name": "Interviewer",
-        "gender": "m"
+      name: "Interviewer",
+      gender: "m",
     },
     {
-        "name": "Ethan Foley",
-        "gender": "m"
+      name: "Ethan Foley",
+      gender: "m",
     },
     {
-        "name": "Terri Mackleberry",
-        "gender": "f"
+      name: "Terri Mackleberry",
+      gender: "f",
     },
     {
-        "name": "Voice",
-        "gender": "m"
+      name: "Voice",
+      gender: "m",
     },
     {
-        "name": "Receptionist",
-        "gender": "f"
+      name: "Receptionist",
+      gender: "f",
     },
     {
-        "name": "Kent Brockman",
-        "gender": "m"
+      name: "Kent Brockman",
+      gender: "m",
     },
     {
-        "name": "Apu Nahasapeemapetilon",
-        "gender": "m"
+      name: "Apu Nahasapeemapetilon",
+      gender: "m",
     },
     {
-        "name": "Lenny Leonard",
-        "gender": "m"
+      name: "Lenny Leonard",
+      gender: "m",
     },
     {
-        "name": "Carl Carlson",
-        "gender": "m"
+      name: "Carl Carlson",
+      gender: "m",
     },
     {
-        "name": "Sideshow Bob",
-        "gender": "m"
+      name: "Sideshow Bob",
+      gender: "m",
     },
     {
-        "name": "Ralph Wiggum",
-        "gender": "m"
+      name: "Ralph Wiggum",
+      gender: "m",
     },
     {
-        "name": "Jimbo Jones",
-        "gender": "m"
+      name: "Jimbo Jones",
+      gender: "m",
     },
     {
-        "name": "Agnes Skinner",
-        "gender": "f"
+      name: "Agnes Skinner",
+      gender: "f",
     },
     {
-        "name": "Kearney Zzyzwicz",
-        "gender": "m"
+      name: "Kearney Zzyzwicz",
+      gender: "m",
     },
     {
-        "name": "DOLPH",
-        "gender": "m"
+      name: "DOLPH",
+      gender: "m",
     },
     {
-        "name": "Judge Snyder",
-        "gender": "m"
+      name: "Judge Snyder",
+      gender: "m",
     },
     {
-        "name": "Jacques",
-        "gender": "m"
+      name: "Jacques",
+      gender: "m",
     },
     {
-        "name": "Salesman",
-        "gender": "m"
+      name: "Salesman",
+      gender: "m",
     },
     {
-        "name": "Woman",
-        "gender": "f"
+      name: "Woman",
+      gender: "f",
     },
     {
-        "name": "Rod Flanders",
-        "gender": "m"
+      name: "Rod Flanders",
+      gender: "m",
     },
     {
-        "name": "Manager",
-        "gender": "m"
+      name: "Manager",
+      gender: "m",
     },
     {
-        "name": "Bleeding Gums Murphy",
-        "gender": "m"
+      name: "Bleeding Gums Murphy",
+      gender: "m",
     },
     {
-        "name": "Cowboy Bob",
-        "gender": "m"
+      name: "Cowboy Bob",
+      gender: "m",
     },
     {
-        "name": "Jacqueline Bouvier",
-        "gender": "f"
+      name: "Jacqueline Bouvier",
+      gender: "f",
     },
     {
-        "name": "Waiter",
-        "gender": "m"
+      name: "Waiter",
+      gender: "m",
     },
     {
-        "name": "Gulliver Dark",
-        "gender": "m"
+      name: "Gulliver Dark",
+      gender: "m",
     },
-]
-
-//örnek çıktı array.groupByCustom(item=>item.gender) için
-/*
-    {
-        "m": [... gender m olan içerik]
-        "f": [... gender f olan içerik]
-    }
- */
-
-    //örnek çıktı array.groupByCustom(item=>item.name[0]) için
-/*
-    {
-        "A": [... name baş harfi A olan içerik]
-        "B": [... name baş harfi B olan içerik]
-        "C": [... name baş harfi C olan içerik]
-        ... devamı
-    }
- */
+  ];
+  
+  array.groupByCustom((i) => i.gender);
+  //örnek çıktı array.groupByCustom(item=>item.gender) için
+  /*
+      {
+          "m": [... gender m olan içerik]
+          "f": [... gender f olan içerik]
+      }
+   */
+  
+  array.groupByCustom((i) => i.name[0]);
+  //örnek çıktı array.groupByCustom(item=>item.name[0]) için
+  /*
+      {
+          "A": [... name baş harfi A olan içerik]
+          "B": [... name baş harfi B olan içerik]
+          "C": [... name baş harfi C olan içerik]
+          ... devamı
+      }
+   */
+  
