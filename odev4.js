42fdc95b76f3d7e899b275d325982fc0ed6ff625
Dilay CANITEZ <dilay.canitez@one37id.com>
2022-08-31 10:59:57 +0300

    denormalizedData fnc added

--- odev4.js
+++ odev4.js
@@ -1,35 +1,62 @@
-/** 
+/**
  * Size verilen iki adet array’den
  * uygun methodları kullanarak yeni
  * bir object oluşturmanız gerekiyor.
- * Ve bu obje key-value şeklinde 
+ * Ve bu obje key-value şeklinde
  * doğru eşleştirme yapılarak
- * oluşturulmalı. 
+ * oluşturulmalı.
  */
 
-
 const input = {
-    "cols": ["Name Surname", "Company", "Email", "Date", "Country", "City"],
-    "data": [
-        ["Hyacinth Vincent", "Duis Corporation", "iaculis.enim@magnaCrasconvallis.ca", "28/06/2022", "Eritrea", "Lyubertsy"],
-        ["Brenden Martinez", "Volutpat Nunc Associates", "iaculis@estMauris.org", "24/03/2021", "British Indian Ocean Territory", "Colwood"]]
-}
+  cols: ["Name Surname", "Company", "Email", "Date", "Country", "City"],
+  data: [
+    [
+      "Hyacinth Vincent",
+      "Duis Corporation",
+      "iaculis.enim@magnaCrasconvallis.ca",
+      "28/06/2022",
+      "Eritrea",
+      "Lyubertsy",
+    ],
+    [
+      "Brenden Martinez",
+      "Volutpat Nunc Associates",
+      "iaculis@estMauris.org",
+      "24/03/2021",
+      "British Indian Ocean Territory",
+      "Colwood",
+    ],
+  ],
+};
+
+const denormalizedData = (data) => {
+  console.log(
+    data.data.map((row) =>
+      data.cols.reduce((prev, curr, indx) => {
+        if (!prev[curr]) prev[curr] = row[indx];
+        return prev;
+      }, {})
+    )
+  );
+};
+
+denormalizedData(input);
 
 const sampleoutput = [
-    {
-        "Name Surname": "Hyacinth Vincent",
-        "Company": "Duis Corporation",
-        "Email": "iaculis.enim@magnaCrasconvallis.ca",
-        "Date": "28/06/2022",
-        "Country": "Eritrea",
-        "City": "Lyubertsy"
-    },
-    {
-        "Name Surname": "Brenden Martinez",
-        "Company": "olutpat Nunc Associates",
-        "Email": "iaculis@estMauris.org",
-        "Date": "24/03/2021",
-        "Country": "British Indian Ocean Territory",
-        "City": "Colwood"
-    }
-]
+  {
+    "Name Surname": "Hyacinth Vincent",
+    Company: "Duis Corporation",
+    Email: "iaculis.enim@magnaCrasconvallis.ca",
+    Date: "28/06/2022",
+    Country: "Eritrea",
+    City: "Lyubertsy",
+  },
+  {
+    "Name Surname": "Brenden Martinez",
+    Company: "olutpat Nunc Associates",
+    Email: "iaculis@estMauris.org",
+    Date: "24/03/2021",
+    Country: "British Indian Ocean Territory",
+    City: "Colwood",
+  },
+];
